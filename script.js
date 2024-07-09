2bc7d0a2cd4ec65250cff38476aaa614d6190372
Akmal <magchuz@pm.me>
2024-07-10 05:18:58 +0700

    Add requestIdleCallback polyfill

--- script.js
+++ script.js
@@ -291,6 +291,20 @@ document.querySelectorAll("th[data-sort]").forEach((header) => {
   });
 });
 
+window.requestIdleCallback =
+  window.requestIdleCallback ||
+  function (cb) {
+    var start = Date.now();
+    return setTimeout(function () {
+      cb({
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        },
+      });
+    }, 1);
+  };
+
 document.addEventListener("DOMContentLoaded", async function () {
   document.getElementById("searchBox").addEventListener("input", searchTable);
   await changeSourceData("data/super.cbor.gz");
